a154ddf956cd37dad6f97f46913f2cc3112d98ca
JNinya <jninya16@gmail.com>
2024-11-19 00:55:51 -0500

    RAIN

--- game.py
+++ game.py
@@ -1,7 +1,37 @@
 import console
+import random
+import time
+
+def randomLine():
+    frequency = 0.1
+    line = ""
+    for i in range(50):
+        if (random.random() < frequency):
+            line += "|"
+        else:
+            line += " "
+    return line
+        
 
 while True:
-    console.interpret(input(":-)"))
+    print(randomLine())
+    time.sleep(0.008)
 
-#riots
-#rain
+
+
+"""
+
+lines = ["riots", "riiiiiiiots", "do you remember", "DO YOU REMEMBER"]
+
+class Riot:
+    def __init__(self, lengthOfIs, lengthOfOs):
+        self.lengthOfIs = lengthOfIs
+        self.lengthOfOs = lengthOfOs
+
+"""
+#DO YOU REMEMBER
+#RIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOT
+
+
+
+
