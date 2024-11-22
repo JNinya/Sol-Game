8bf30856250a61a313498b826aa235c53c3ebf36
JNinya <jninya16@gmail.com>
2024-11-22 15:30:37 -0500

    finished working for now

--- game.py
+++ game.py
@@ -1,18 +1,11 @@
 from saveload import Saveload
+import glob
 from gameobjects.character import Character
 from gameobjects.location import Location
 
 
-"""me = Character()
-me.createNewFromUser()
-me.save()
-print(me.dict)
 
-mars = Location()
-mars.createNewFromUser()
-mars.save()
-print(mars.dict)"""
-
-luna = Location()
-luna.load("Luna")
-print(luna.dict)
+maxo = Location()
+maxo.createNewFromUser()
+print(maxo.dict)
+maxo.save()
---
+++ savedata/events/blowupmoon.py
@@ -0,0 +1,2 @@
+class Blowupmoon():
+    pass
