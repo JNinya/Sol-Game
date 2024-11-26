cea9cafe8461c1414be862c37fd7a9bab3b073e3
JNinya <jninya16@gmail.com>
2024-11-26 00:02:40 -0500

    messed around with saved characters

--- util/gameloop/render.py
+++ util/gameloop/render.py
@@ -10,5 +10,19 @@ class Render():
         os.system('cls')
 
 
+        characters_at_location = Catalog.search({
+            "type":"Character",
+            "location":me.location
+        })
+
+        characters_at_location.remove(me)
+
+        characters_at_location_str = ""
+
+        for item in characters_at_location:
+            characters_at_location_str += "\n" + item.name
+
+
         print(f"You are at {me.location}")
+        print(f"Other people at {me.location}: {characters_at_location_str}")
         print("Where do you want to go?")
